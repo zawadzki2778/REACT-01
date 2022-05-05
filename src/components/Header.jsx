72466baa3a35d6144ee57cef88960bf49b8940e2
zawadzki2778 <zawadzki2778@gmail.com>
2022-05-05 22:40:43 +0300

    created a components folder + 3 files in it

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://cdn.shopify.com/shopifycloud/hatchful_web_two/bundles/4a14e7b2de7f6eaf5a6c98cb8c00b8de.png" alt="levis" />
+        </header>
+    )
+};
+
+export default Header;
