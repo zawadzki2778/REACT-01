ab72744c5f3b433e7a6eefc658ae3dd48abb9799
zawadzki2778 <zawadzki2778@gmail.com>
2022-05-06 19:38:28 +0300

    created 2 files module.css and one just css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src="https://cdn.shopify.com/shopifycloud/hatchful_web_two/bundles/4a14e7b2de7f6eaf5a6c98cb8c00b8de.png" alt="levis" />
         </header>
     )
