2cdda878691cf655a5658a2eb2843bb6fff037c3
zawadzki2778 <zawadzki2778@gmail.com>
2022-05-07 21:54:26 +0300

    created news components and routes: Music, News and Settings

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -16,6 +19,9 @@ const App = () => {
           <Routes>
             <Route path="/profile" element={<Profile/>}/>
             <Route path="/dialogs" element={<Dialogs/>}/>
+            <Route path="/news" element={<News/>}/>
+            <Route path="/music" element={<Music/>}/>
+            <Route path="/settings" element={<Settings/>}/>
           </Routes>  
         </div>
       </div>
