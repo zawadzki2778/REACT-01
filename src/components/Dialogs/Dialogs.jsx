c0623e914a0dfc56edfa40192fd5f6f2a81ae850
zawadzki2778 <zawadzki2778@gmail.com>
2022-05-08 19:42:27 +0300

    l21: filled the component Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,20 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Maksim</div>
+                <div className={s.dialog + ' ' + s.active}>Vita</div>
+                <div className={s.dialog}>Denis</div>
+                <div className={s.dialog}>Evgeniy</div>
+                <div className={s.dialog}>Julick</div>
+                <div className={s.dialog}>Vera</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello!</div>
+                <div className={s.message}>How is your react learning?</div>
+                <div className={s.message}>Yo, its supper</div>
+            </div>
         </div>
     )
 };
