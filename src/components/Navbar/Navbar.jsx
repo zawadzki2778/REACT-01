29eca0dbe518693793324a6fd9e2be908ceef362
zawadzki2778 <zawadzki2778@gmail.com>
2022-05-08 15:03:20 +0300

    l20: made active links in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import s from'./Navbar.module.css';
+import s from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <nav className={s.nav}>
-        <div className={s.item}>
-          <a href="/profile">Profile</a>
-        </div>
-        <div className={s.item}>
-          <a href="/dialogs">Messages</a>
-          </div>
-        <div className={s.item}>
-          <a href="/news">News</a>
-          </div>
-        <div className={s.item}>
-          <a href="/music">Music</a>
-          </div>
-          <div className={s.item}>
-          <a href="/settings">Settings</a>
-          </div>
-      </nav>
-    )
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <NavLink to="/profile" className={ navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
+      </div>
+       <div className={s.item}> {/* {`${s.item} ${s.active}`}  */}
+        <NavLink to="/dialogs" className={ navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to ="/news" className={ navData => navData.isActive ? s.active : s.item }>News</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/music" className={ navData => navData.isActive ? s.active : s.item }>Music</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/settings" className={ navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
+      </div>
+    </nav>
+  )
 };
 
 export default Navbar;
+// можно вынести в отдельную ф-ию:
+// const SelectedLink = () => {
+//   return (
+//       select => select.isActive ? s.activeLink : s.item
+//   ); - целый кусок кода.
+
+// className={SelectedLink()} - кусочек кода.
