fde0acb9e33251644bbb303b9e4e247a51e8d75f
zawadzki2778 <zawadzki2778@gmail.com>
2022-05-09 21:11:19 +0300

    l26: split the dialogs component into 2 sub-components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
 
-const DialogItem = (props) => {
-    const path = "/dialogs/" + props.id;
-    return (
-        <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-};
-const Message = (props) => {
-    return (
-        <div className={s.dialog}>{props.message}</div>
-    )
-};
 const Dialogs = (props) => {
     let dialogs = [
         { id: 1, name: 'Максим' },
