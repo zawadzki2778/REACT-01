fb099049244ec278b810a478517ae4fabaeecc3a
zawadzki2778 <zawadzki2778@gmail.com>
2022-05-10 19:33:49 +0300

    fixed paths in imports Message and DialogItem

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import s from './Dialogs.module.css';
+import s from './../Dialogs.module.css';
 
 const DialogItem = (props) => {
     const path = "/dialogs/" + props.id;
